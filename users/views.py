04c106f164459d01e6fd973b217ebac475c5ba55
tolgakameroglu <tolga_kameroglu@hotmail.com>
2022-10-19 12:04:20 +0300

    changes

--- users/views.py
+++ users/views.py
@@ -12,8 +12,8 @@ def index(request):
 
 def login_view(request):
     if request.method == "POST":
-        username = request.POST["username"]
-        password = request.POST["password"]
+        username = request.POST.get("username")
+        password = request.POST.get("password")
         user = authenticate(request, username=username, password=password)
 
         if user is not None:
